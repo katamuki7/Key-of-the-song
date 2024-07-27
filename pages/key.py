97d95059c625ed6ecc2c420333a917b59d7a05b9
katamuki <66561440+katamuki7@users.noreply.github.com>
2024-07-27 15:48:22 +0900

    Update key.py
    ChatGPTのモデルを変更

--- pages/key.py
+++ pages/key.py
@@ -11,7 +11,7 @@ oto_ary=["C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", 
          "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
          "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6", "C7"]
 
-model = "gpt-3.5-turbo"
+model = "gpt-4o-mini"
 
 my_saitei_cnt = 0
 saitei_cnt = 0
